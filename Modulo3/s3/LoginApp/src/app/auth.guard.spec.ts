5f30742629bbea0a2ce0cb8198b8a789f9de3dee
Guido Mallamace <127422551+GMallamace@users.noreply.github.com>
2023-06-12 02:04:27 +0200

    Add files via upload

---
+++ Modulo3/s3/LoginApp/src/app/auth.guard.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+import { CanActivateChildFn } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+
+describe('AuthGuard', () => {
+  const executeGuard: CanActivateChildFn = (...guardParameters) => 
+      TestBed.runInInjectionContext(() => AuthGuard(...guardParameters));
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+  });
+
+  it('should be created', () => {
+    expect(executeGuard).toBeTruthy();
+  });
+});
